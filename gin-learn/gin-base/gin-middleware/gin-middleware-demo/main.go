14af65f26e13f2e8fecede779291aef2a11d6fa7
cjq <3472189068@qq.com>
2023-01-29 09:59:15 +0800

    gin-learn

--- gin-learn/gin-base/gin-middleware/gin-middleware-demo/main.go
+++ gin-learn/gin-base/gin-middleware/gin-middleware-demo/main.go
@@ -23,8 +23,9 @@ func main() {
 			"msg": "success",
 		})
 	})
+	// 按顺序执行局部中间件如果由全局中间件先执行外部的全局中间件
 	// 中间件局部注册
-	r.GET("/hello2", MiddleWare(), func(c *gin.Context) {
+	r.GET("/hello2", MiddleWare, func(c *gin.Context) {
 		fmt.Println("执行了Get方法")
 		c.JSON(http.StatusOK, gin.H{
 			"msg": "success",
